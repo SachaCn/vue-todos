e9f129284ff20442314e7ec63f606819fe402db8
SachaCn <sachacrn@gmail.com>
2023-09-11 14:13:29 +0200

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="about">
+    <h1>Vue Todos</h1>
+    <br>
+    <p>Vue Todos is a CRUD Todo tracking application created for a Vue 3 Crash Course by John Komarnicki</p>
+    <br>
+    <p>Track you daily task's using this todo tracker cith abilities to save, complete, edit & delete todos</p>
+    <br>
+    <p>Feel free to add on to this application with functionalities such as the ability to drag and reorder todos, animate creation/deletion of todos or even create this into a full stack application with the ability to hace different lists of different days of tracking todos8</p>
+  </div>
+</template>
+
+<style>
+.about {
+    padding: 20px 15px;
+  }
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+    padding: 20px 5px;
+  }
+}
+</style>
